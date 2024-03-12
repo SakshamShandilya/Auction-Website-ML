7acee3a6bf2f3392dacce62cd773ce785c80586c
SAKSHAM SHANDILYA <saksham140023@gmail.com>
2024-03-12 11:14:25 +0530

    Updated Footer.tsx

--- components/Base/Footer.tsx
+++ components/Base/Footer.tsx
@@ -142,17 +142,15 @@ const Footer = (props: Props) => {
                         >
                             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
                         </svg>
-                        <span className="ml-3 text-xl">Tailblocks</span>
+                        <span className="ml-3 text-xl">114 Group</span>
                     </a>
                     <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
                         © 2024 Tailblocks —
                         <a
-                            href="https://twitter.com/knyttneve"
                             rel="noopener noreferrer"
                             className="text-gray-600 ml-1"
                             target="_blank"
                         >
-                            @knyttneve
                         </a>
                     </p>
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
